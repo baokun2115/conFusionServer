6a31795fa9a58548ed9a64b32a519a9ad21072b5
giabao <baotran2115@gmail.com>
2023-06-18 15:53:39 +0700

    Mongoose Population

--- routes/users.js
+++ routes/users.js
@@ -24,10 +24,17 @@ router.post("/signup", (req, res, next) => {
         res.setHeader("Content-Type", "application/json");
         res.json({ err: err });
       } else {
-        passport.authenticate("local")(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json({ success: true, status: "Registration Successful!" });
+        if (req.body.firstname) user.firstname = req.body.firstname;
+        if (req.body.lastname) user.lastname = req.body.lastname;
+        user.save().then((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+          }
+          passport.authenticate("local")(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ success: true, status: "Registration Successful!" });
+          });
         });
       }
     }
@@ -44,6 +51,17 @@ router.post("/login", passport.authenticate("local"), (req, res, next) => {
     status: "You are successfully logged in!",
   });
 });
+
+// router.post("/login", passport.authenticate("local"), (req, res, next) => {
+//   var token = authenticate.getToken({ _id: req.user._id });
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "application/json");
+//   res.json({
+//     success: true,
+//     token: token,
+//     status: "You are successfully logged in!",
+//   });
+// });
 
 router.get("/logout", (req, res, next) => {
   if (req.session) {
